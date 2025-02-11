3815cb6e0308092b3e0a695cc8773339c9d2a3a2
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2025-02-11 18:26:55 +0800

    Initial try.

--- QJSONConfig/qjsonconfig.h
+++ QJSONConfig/qjsonconfig.h
@@ -1,1 +1,21 @@
+// QJSONConfig -- A Qt JSON config parser
 
+#include <QString>
+#include <QSettings>
+#include <QVariant>
+
+class QJSONConfig
+{
+private:
+    QString m_cfgFile;
+    QSettings m_interSettings;
+public:
+    explicit ConfigManager(const QString &filePath = "config.json")
+        : m_cfgFile(filePath) {}
+    bool read();
+    bool save() const;
+    QVariant getValue(const QString& key, const QVariant& defaultValue = QVariant()) const;
+    void setValue(const QString& key, const QVariant& value);
+    QVariant &operator[](const QString &cfgKey);
+    
+};
