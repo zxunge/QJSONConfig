1fad15b23f56ddd300db6227732b091fd3d7beb8
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2025-02-11 21:41:28 +0800

    Improve code.

--- QJSONConfig/qjsonconfig.h
+++ QJSONConfig/qjsonconfig.h
@@ -14,14 +14,38 @@ private:
     QSettings *m_interSettings;
 
 public:
-    explicit ConfigManager(const QString &filePath = "config.json")
+    // Client class: distinguish between lvalue and rvalue
+    class Proxy {
+    public:
+        Proxy(QVariant& container, QString cfgKry) 
+            : container(container),key(cfgKey) {}
+
+        // Convert into value (rvalue)
+        operator QVariant() const {
+            return container;
+        }
+
+        // Assignment (lvalue)
+        Proxy& operator=(QVariant value) {
+            m_interSettings->setValue(key, value);
+            return *this;
+        }
+
+    private:
+        QVariant& container;
+        QString key;
+    };
+
+public:
+    explicit QJSONConfig(const QString &filePath = "config.json")
         : m_cfgFile(filePath) { QSettings::Format format { QSettings::registerFormat("json", &readFunc, &writeFunc) }; m_interSettings = new QSettings(filePath, format); }
+    ~QJSONConfig() { delete m_interSettings; }
     
     bool sync();
     void clear();
     QVariant getValue(const QString& key, const QVariant& defaultValue = QVariant()) const;
     void setValue(const QString& key, const QVariant& value);
-    QVariant &operator[](const QString &cfgKey);
+    Proxy &operator[](const QString &cfgKey) { return Proxy(getValue(cfgKey), cfgKey); }
 
 private:
     bool readFunc(QIODevice &device, QSettings::SettingsMap &map);
