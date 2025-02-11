911be5d2f7e3177a8c9092878a75b610e2b3c4a6
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2025-02-11 23:24:34 +0800

    Update demo.

--- demo/main.cpp
+++ demo/main.cpp
@@ -7,20 +7,33 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Set information gor QSettings.
     QCoreApplication::setOrganizationName("MySoft");
     QCoreApplication::setOrganizationDomain("mysoft.com");
     QCoreApplication::setApplicationName("Star Runner");
-   
-    QJSONConfig cfg;
+
+    // Declare your config instance
+    QJSONConfig cfg("conf.json" /* File Name */);
 
     // Write configurations
+    // Currently we only support 4 types of the value of each config item:
+    // int, QString, double, bool
+    // It is ok by setValue(QString key, QVariant val).
     cfg["name"] = "name";
     cfg["num"] = 1;
+    cfg["Boolean"] = true;
+    cfg["float"] = 1.8;
+
+    // Syncronize the config file
     cfg.sync();
 
     // Read
+    // Similarly we support getValue(WString key).
     qDebug() << cfg["name"];
     qDebug() << cfg["num"];
+    qDebug() << cfg["Boolean"];
+    qDebug() << cfg["float"];
 
     return 0;
 }
