d7689b68a8078bc701c12d72d4ff9db97dc36739
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2025-02-11 21:31:54 +0800

    Create demo.

---
+++ demo/main.cpp
@@ -0,0 +1,25 @@
+// QJSONConfig Demo
+
+#include <QApplication>
+#include "qjsonconfig.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QCoreApplication::setOrganizationName("MySoft");
+    QCoreApplication::setOrganizationDomain("mysoft.com");
+    QCoreApplication::setApplicationName("Star Runner");
+   
+    QJSONConfig cfg;
+
+    // Write configurations
+    cfg["name"] = "name";
+    cfg["num"] = 1;
+    cfg.sync();
+
+    // Read
+    qDebug() << cfg["name"];
+    qDebug() << cfg["num"];
+
+    return a.exec();
+}
