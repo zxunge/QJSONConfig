78328e8508d255d2468df789adb0ddf56b7c24f7
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2025-02-11 18:45:06 +0800

    Update qjsonconfig.h

--- QJSONConfig/qjsonconfig.h
+++ QJSONConfig/qjsonconfig.h
@@ -1,21 +1,29 @@
 // QJSONConfig -- A Qt JSON config parser
+// By: zxunge 
+// Available at: https://github.com/zxunge/QJSONConfig
 
 #include <QString>
 #include <QSettings>
 #include <QVariant>
+#include <QIODevice>
 
 class QJSONConfig
 {
 private:
     QString m_cfgFile;
-    QSettings m_interSettings;
+    QSettings *m_interSettings;
+
 public:
     explicit ConfigManager(const QString &filePath = "config.json")
-        : m_cfgFile(filePath) {}
+        : m_cfgFile(filePath) { QSettings::Format format { QSettings::registerFormat("json", &readFunc, &writeFunc) }; m_interSettings = new QSettings(filePath, format); }
     bool read();
     bool save() const;
     QVariant getValue(const QString& key, const QVariant& defaultValue = QVariant()) const;
     void setValue(const QString& key, const QVariant& value);
     QVariant &operator[](const QString &cfgKey);
-    
+
+private:
+    bool readFunc(QIODevice &device, QSettings::SettingsMap &map);
+    bool writeFunc(QIODevice &device, const QSettings::SettingsMap &map);
+    bool exists(QString key);
 };
