2a78470dfde0f98f400ec1c57c008d25baeb815c
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2025-02-15 22:15:18 +0800

    Improve.

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,6 +1,9 @@
 // QJSONConfig Demo
 
 #include <QApplication>
+// If you want to use the unreliable Proxy class to 
+// acheive "convenient access"(i.e. use operator[] to acheive both reading and writing), you need to
+// define QJSONCFG_PROXY_USED before including the header.
 #include "qjsonconfig.h"
 #include <QTextStream>
 #include <iostream>
@@ -22,44 +25,42 @@ int main(int argc, char *argv[])
     out << cfg.empty() << Qt::endl;
     
     // Write configurations
-    // Currently we only support 4 types of the value of each config item:
-    // int, QString, double, bool
-    // It is ok by setValue(QString key, QVariant val).
-    cfg["String"] = "name";
-    cfg["Integer"] = 1;
-    cfg["Boolean"] = true;
-    cfg["Float"] = 1.8;
+    // It is not suggested to directly use operator[] (if QJSONCFG_PROXY_USED is defined)
+    cfg.setValue("String", "name");
+    cfg.setValue("Integer", 1);
+    cfg.setValue("Boolean", true);
+    cfg.setValue("Float", 1.8);
 
     // We also support sub-configuration
-    cfg["Foo/Bar"] = 250;
-    cfg["Foo/Boo"] = 150;
-    cfg["Foo/Bar/Boo"] = 200;
+    cfg.setValue("Foo/Bar", 250);
+    cfg.setValue("Foo/Boo", 150);
+    cfg.setValue("Foo/Bar/Boo", 200);
 
     // Syncronize the config file
     cfg.sync();
 
     // Read
     // Similarly we support getValue(QString key)
-    out << QString(cfg["String"]) << Qt::endl;
-    out << int(cfg["Integer"]) << Qt::endl;
-    out << bool(cfg["Boolean"]) << Qt::endl;
-    out << double(cfg["Float"]) << Qt::endl;
-    out << int(cfg["Foo/Bar"]) << Qt::endl;
-    out << int(cfg["Foo/Boo"]) << Qt::endl;
-    out << int(cfg["Foo/Bar/Boo"]) << Qt::endl;
+    out << cfg["String"] << Qt::endl;
+    out << cfg["Integer"] << Qt::endl;
+    out << cfg["Boolean"] << Qt::endl;
+    out << cfg["Float"] << Qt::endl;
+    out << cfg["Foo/Bar"] << Qt::endl;
+    out << cfg["Foo/Boo"] << Qt::endl;
+    out << cfg["Foo/Bar/Boo"] << Qt::endl;
 
     // Clear all the configuration
     // cfg.clear();
 
     // Now use another to read those config items
     QJSONConfig cfgRead("conf.json");
-    out << QString(cfgRead["String"]) << Qt::endl;
-    out << int(cfgRead["Integer"]) << Qt::endl;
-    out << bool(cfgRead["Boolean"]) << Qt::endl;
-    out << double(cfgRead["Float"]) << Qt::endl;
-    out << int(cfgRead["Foo/Bar"]) << Qt::endl;
-    out << int(cfgRead["Foo/Boo"]) << Qt::endl;
-    out << int(cfgRead["Foo/Bar/Boo"]) << Qt::endl;
+    out << cfgRead["String"] << Qt::endl;
+    out << cfgRead["Integer"] << Qt::endl;
+    out << cfgRead["Boolean"] << Qt::endl;
+    out << cfgRead["Float"] << Qt::endl;
+    out << cfgRead["Foo/Bar"] << Qt::endl;
+    out << cfgRead["Foo/Boo"] << Qt::endl;
+    out << cfgRead["Foo/Bar/Boo"] << Qt::endl;
 
     return 0;
 }
